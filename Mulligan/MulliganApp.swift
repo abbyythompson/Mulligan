cd5fabc08dec6b0524ea3926f79367039ccfb831
Abby Thompson <abbythompson@Abbys-MacBook-Air.local>
2025-06-26 11:13:56 +0100

    initial commit

---
+++ Mulligan/MulliganApp.swift
@@ -0,0 +1,17 @@
+//
+//  MulliganApp.swift
+//  Mulligan
+//
+//  Created by Abby Thompson on 26.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct MulliganApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MulliganUITests/MulliganUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  MulliganUITestsLaunchTests.swift
+//  MulliganUITests
+//
+//  Created by Abby Thompson on 26.06.2025.
+//
+
+import XCTest
+
+final class MulliganUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
